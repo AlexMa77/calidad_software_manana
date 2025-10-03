b5339b0bc3072a4a1fa061dd55c42a7a1dc01fe2
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-10-03 09:52:51 -0500

    programador macias

--- modulo_1/git/myapp.py
+++ modulo_1/git/myapp.py
@@ -2,4 +2,6 @@ print ("PRUEBA DE CALIDAD DE SOFTWARE")
 materia= "Calidad de software"
 if (materia):
     print ("Estoy practicando git en: ", materia)
+else: 
+    print ("Es otra materia ")
     
