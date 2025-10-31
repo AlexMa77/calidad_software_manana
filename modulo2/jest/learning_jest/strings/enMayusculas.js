f2f66de923ccba19fbac1cc1482aaad3b7717751
AlexMa77 <am9614843@gmail.com>
2025-10-31 10:10:26 -0500

    Clase 2025/10/31

---
+++ modulo2/jest/learning_jest/strings/enMayusculas.js
@@ -0,0 +1,8 @@
+function enMayusculas(texto) {
+    if (!texto || typeof texto !== 'string') {
+        throw new TypeError("texto inválido");
+    }
+    return texto.toUpperCase();
+}
+
+module.exports = { enMayusculas };
